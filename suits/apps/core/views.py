27d95b39adab136faf9a2b83164415d92debb1d4
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2026-03-17 13:09:43 +0200

    Implement api_root view for API information
    Add API root endpoint returning available endpoints

--- suits/apps/core/views.py
+++ suits/apps/core/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+
+@require_GET
+def api_root(request):
+  return JsonResponse({
+    "message": "Suits API Backend",
+    "Version": "",
+    "endpoints": {
+      "admin": "/admin/",
+      "api": "/api/",
+      "auth": "/api/auth/login",
+      "token_refresh": "/api/auth/refresh",
+      "tenants": "/api/tenants",
+      "lawfirms": "/api/lawfirms",
+      "attorneys": "/api/attorneys",
+      "clients": "/api/clients",
+      "cases": "/api/cases",
+      "documents": "/api/documents",
+      "users": "/api/users",
+      "cases": "/api/cases",
+      "workflows": "/api/workflows-templates",
+      "workflowSteps": "/api/steps",
+      "workflowTransitions": "/api/transitions",
+    }
+  })
+
